b3bd7ab48c6c3d8b418f2f7b4cf2ad15fbdbd71d
alechp <me@alechp.com>
2024-09-08 08:41:41 -0700

    placeholder; going to migrate these into repos to test installing from source

--- examples/plugins/gs_templates/gs_templates.go
+++ examples/plugins/gs_templates/gs_templates.go
@@ -1,67 +1,155 @@
+// gs_templates.go
 package main
 
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/hashicorp/hcl/v2/hclsimple"
-	"github.com/ssotspace/gitspace/lib"
+	"github.com/charmbracelet/huh"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type TemplatePlugin struct{}
 
-type TemplateConfig struct {
-	Template struct {
-		Name        string   `hcl:"name,label"`
-		Version     string   `hcl:"version"`
-		Description string   `hcl:"description"`
-		Author      string   `hcl:"author"`
-		Files       []string `hcl:"files,optional"`
-		Tokens      []struct {
-			Name     string   `hcl:"name"`
-			Files    []string `hcl:"files"`
-			Encoding string   `hcl:"encoding"`
-			Phase    string   `hcl:"phase"`
-		} `hcl:"tokens,block"`
-		ChildTemplates []string `hcl:"child_templates,optional"`
-		Parent         string   `hcl:"parent,optional"`
-	} `hcl:"template,block"`
-}
-
 func (t TemplatePlugin) Run() error {
-	// Get the template path from the user
-	templatePath, err := lib.GetPathWithCompletion("Enter the path to the template file (default: gs.template.hcl): ")
-	if err != nil {
-		return fmt.Errorf("error getting template path: %w", err)
-	}
-	if templatePath == "" {
-		templatePath = "gs.template.hcl"
-	}
+	for {
+		var choice string
+		err := huh.NewSelect[string]().
+			Title("Template Plugin Menu").
+			Options(
+				huh.NewOption("Generate New Template", "generate"),
+				huh.NewOption("Install Templates", "install"),
+				huh.NewOption("Print Installed Templates Summary", "summary"),
+				huh.NewOption("Exit", "exit"),
+			).
+			Value(&choice).
+			Run()
 
-	// Parse the template
-	config, err := parseTemplate(templatePath)
-	if err != nil {
-		return fmt.Errorf("error parsing template: %w", err)
-	}
+		if err != nil {
+			return fmt.Errorf("error getting user choice: %w", err)
+		}
 
-	// Print summary of the template
-	printTemplateSummary(config)
+		switch choice {
+		case "generate":
+			err = generateNewTemplate()
+		case "install":
+			err = installTemplates()
+		case "summary":
+			err = printInstalledTemplatesSummary()
+		case "exit":
+			return nil
+		}
 
-	// If it's a parent template, parse and print summaries of child templates
-	if len(config.Template.ChildTemplates) > 0 {
-		for _, childName := range config.Template.ChildTemplates {
-			childPath := filepath.Join(filepath.Dir(templatePath), childName, "template.child."+config.Template.Name+"."+childName+".gs.hcl")
-			childConfig, err := parseTemplate(childPath)
-			if err != nil {
-				fmt.Printf("Error parsing child template %s: %v\n", childName, err)
-				continue
-			}
-			fmt.Printf("\nChild Template: %s\n", childName)
-			printTemplateSummary(childConfig)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
 		}
 	}
+}
 
+func generateNewTemplate() error {
+	var templateType string
+	err := huh.NewSelect[string]().
+		Title("Select template type").
+		Options(
+			huh.NewOption("Child", "child"),
+			huh.NewOption("Parent", "parent"),
+		).
+		Value(&templateType).
+		Run()
+
+	if err != nil {
+		return fmt.Errorf("error getting template type: %w", err)
+	}
+
+	var name, version, description, author, entryPoint, sourceType, repository, branch string
+
+	err = huh.NewForm().
+		Title("Template Information").
+		Field(huh.NewInput().Title("Name").Value(&name)).
+		Field(huh.NewInput().Title("Version").Value(&version)).
+		Field(huh.NewInput().Title("Description").Value(&description)).
+		Field(huh.NewInput().Title("Author").Value(&author)).
+		Field(huh.NewInput().Title("Entry Point").Value(&entryPoint)).
+		Field(huh.NewInput().Title("Source Type").Value(&sourceType)).
+		Field(huh.NewInput().Title("Repository").Value(&repository)).
+		Field(huh.NewInput().Title("Branch").Value(&branch)).
+		Run()
+
+	if err != nil {
+		return fmt.Errorf("error getting template information: %w", err)
+	}
+
+	// Generate HCL file
+	f := hclwrite.NewEmptyFile()
+	rootBody := f.Body()
+	pluginBlock := rootBody.AppendNewBlock("plugin", []string{name})
+	pluginBody := pluginBlock.Body()
+
+	pluginBody.SetAttributeValue("version", cty.StringVal(version))
+	pluginBody.SetAttributeValue("description", cty.StringVal(description))
+	pluginBody.SetAttributeValue("author", cty.StringVal(author))
+	pluginBody.SetAttributeValue("entry_point", cty.StringVal(entryPoint))
+
+	sourceBlock := pluginBody.AppendNewBlock("source", nil)
+	sourceBody := sourceBlock.Body()
+	sourceBody.SetAttributeValue("type", cty.StringVal(sourceType))
+	sourceBody.SetAttributeValue("repository", cty.StringVal(repository))
+	sourceBody.SetAttributeValue("branch", cty.StringVal(branch))
+
+	fileName := fmt.Sprintf("template.%s.%s.gs.hcl", templateType, name)
+	err = os.WriteFile(fileName, f.Bytes(), 0644)
+	if err != nil {
+		return fmt.Errorf("error writing template file: %w", err)
+	}
+
+	fmt.Printf("Template generated: %s\n", fileName)
+	return nil
+}
+
+func installTemplates() error {
+	var choice string
+	err := huh.NewSelect[string]().
+		Title("Select Source").
+		Options(
+			huh.NewOption("Local Path", "local"),
+			huh.NewOption("Remote Repository", "remote"),
+		).
+		Value(&choice).
+		Run()
+
+	if err != nil {
+		return fmt.Errorf("error getting source choice: %w", err)
+	}
+
+	switch choice {
+	case "local":
+		path, err := lib.GetPathWithCompletion("Enter local path to template: ")
+		if err != nil {
+			return fmt.Errorf("error getting local path: %w", err)
+		}
+		// Here you would implement the logic to install from a local path
+		fmt.Printf("Installing template from local path: %s\n", path)
+	case "remote":
+		var repo string
+		err = huh.NewInput().
+			Title("Enter repository URL").
+			Value(&repo).
+			Run()
+		if err != nil {
+			return fmt.Errorf("error getting repository URL: %w", err)
+		}
+		// Here you would implement the logic to install from a remote repository
+		fmt.Printf("Installing template from remote repository: %s\n", repo)
+	}
+
+	return nil
+}
+
+func printInstalledTemplatesSummary() error {
+	// This function would scan the installed templates directory and print a summary
+	fmt.Println("Installed Templates Summary:")
+	// Implement the logic to scan and summarize installed templates
 	return nil
 }
 
@@ -71,36 +159,6 @@ func (t TemplatePlugin) Name() string {
 
 func (t TemplatePlugin) Version() string {
 	return "1.0.0"
-}
-
-func parseTemplate(path string) (*TemplateConfig, error) {
-	var config TemplateConfig
-	err := hclsimple.DecodeFile(path, nil, &config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode HCL: %w", err)
-	}
-	return &config, nil
-}
-
-func printTemplateSummary(config *TemplateConfig) {
-	fmt.Printf("Template Name: %s\n", config.Template.Name)
-	fmt.Printf("Version: %s\n", config.Template.Version)
-	fmt.Printf("Description: %s\n", config.Template.Description)
-	fmt.Printf("Author: %s\n", config.Template.Author)
-	if config.Template.Parent != "" {
-		fmt.Printf("Parent: %s\n", config.Template.Parent)
-	}
-	fmt.Printf("Files: %v\n", config.Template.Files)
-	fmt.Printf("Tokens:\n")
-	for _, token := range config.Template.Tokens {
-		fmt.Printf("  - Name: %s\n", token.Name)
-		fmt.Printf("    Files: %v\n", token.Files)
-		fmt.Printf("    Encoding: %s\n", token.Encoding)
-		fmt.Printf("    Phase: %s\n", token.Phase)
-	}
-	if len(config.Template.ChildTemplates) > 0 {
-		fmt.Printf("Child Templates: %v\n", config.Template.ChildTemplates)
-	}
 }
 
 // Export the plugin
